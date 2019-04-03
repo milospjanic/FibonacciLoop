03af063295ebd9a8b30341f173fe1a55f2986c04
Milos Pjanic <pjanic.milos@gmail.com>
2019-04-03 10:27:08 -0700

    Update FibonacciLoop.sh

--- FibonacciLoop.sh
+++ FibonacciLoop.sh
@@ -1,32 +1,36 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Tue Mar 12 05:32:45 2019
+Created on Tue Mar 12 05:22:28 2019
 
 @author: milospjanic
 """
 
-# Python program to display the Fibonacci sequence up to n-th term using recursive functions
+# Program to display the Fibonacci sequence up to n-th term where n is provided by the user
 
-def recur_fibo(n):
-   """Recursive function to
-   print Fibonacci sequence"""
-   if n <= 1:
-       return n
-   else:
-       return(recur_fibo(n-1) + recur_fibo(n-2))
-
-# Change this value for a different result
+# change this value for a different result
 #nterms = 10
 
 # uncomment to take input from the user
 nterms = int(input("How many terms? "))
 
+# first two terms
+n1 = 0
+n2 = 1
+count = 0
+
 # check if the number of terms is valid
 if nterms <= 0:
-   print("Plese enter a positive integer")
+   print("Please enter a positive integer")
+elif nterms == 1:
+   print("Fibonacci sequence upto",nterms,":")
+   print(n1)
 else:
-   print("Fibonacci sequence:")
-   for i in range(nterms):```
-       print(recur_fibo(i), end=" , ")
-
+   print("Fibonacci sequence upto",nterms,":")
+   while count < nterms:
+       print(n1,end=" , ")
+       n3 = n1 + n2
+       # update values
+       n1 = n2
+       n2 = n3
+       count += 1
